1da01e424d432910ef5625cd4996f76a7551d578
Grigoriy <grishatinker@gmail.com>
2023-01-09 13:17:37 +0300

    added and stylized video section

--- js/main.js
+++ js/main.js
@@ -5,3 +5,23 @@ $(document).ready(function(){
 		loop: true,
 	});
 });
+
+// Video
+
+const videoBtn = document.querySelector('#video-btn');
+const videoPreview = document.querySelector('.video-preview');
+const videoWrapper = document.querySelector('.video');
+const video = document.querySelector('#video-file');
+
+videoWrapper.addEventListener('click', () => {
+	if (video.paused) {
+		videoPreview.classList.add('none');
+		videoWrapper.classList.remove('video-overlay')
+		videoBtn.classList.add('none');
+		video.play();
+	} else {
+		videoWrapper.classList.add('video-overlay')
+		videoBtn.classList.remove('none');
+		video.pause();
+	}
+})
