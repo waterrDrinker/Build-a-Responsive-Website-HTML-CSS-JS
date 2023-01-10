480cb0a39d5d2c396519f50bb553fc32420571fb
Grigoriy <grishatinker@gmail.com>
2023-01-10 22:06:45 +0300

    added mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
-		dots: false,
-		margin: 48,
-		loop: true,
+		responsive : {
+			// breakpoint from 0 up
+			0 : {
+					items: 2,
+					margin: 32,
+					loop: true,
+					dots: false,
+			},
+			// breakpoint from 480 up
+			601 : {
+					items: 2,
+					margin: 32,
+					loop: true,
+					dots: false,
+			},
+			// breakpoint from 768 up
+			1001 : {
+					items: 3,
+					margin: 48,
+					loop: true,
+					dots: false,
+			}
+	}
 	});
 });
 
@@ -15,7 +35,7 @@ const video = document.querySelector('#video-file');
 
 videoWrapper.addEventListener('click', () => {
 	if (video.paused) {
-		videoPreview.classList.add('none');
+		videoPreview.classList.add('hidden');
 		videoWrapper.classList.remove('video-overlay')
 		videoBtn.classList.add('none');
 		video.play();
@@ -25,3 +45,18 @@ videoWrapper.addEventListener('click', () => {
 		video.pause();
 	}
 })
+
+// Mobile nav
+const openNavBtn = document.querySelector('#openMobileNav');
+const closeNavBtn = document.querySelector('#closeMobileNav');
+const mobileNav = document.querySelector('#mobileNav');
+
+openNavBtn.onclick = () => {
+	mobileNav.classList.remove('none');
+	document.body.classList.add('no-scroll');
+}
+
+closeNavBtn.onclick = () => {
+	mobileNav.classList.add('none');
+	document.body.classList.remove('no-scroll');
+}
